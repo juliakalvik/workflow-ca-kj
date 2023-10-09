2ba04aa8b6cad95dcb1a3078ff1750376190cb10
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-10-09 11:41:00 +0200

    login-form

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,13 @@
+import LoginForm from "../components/login-form";
 /**
  * Contains form for logging a registered user profile.
  * @see https://docs.noroff.dev/social-endpoints/authentication
  */
+
 export default function LoginPage() {
   return (
     <>
-      <h1>Login Page</h1>
+      <LoginForm />
     </>
   );
 }
