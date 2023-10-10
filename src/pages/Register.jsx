48c9125f335737a95eb6d2cfaedaca937ae6a710
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-10-10 13:28:40 +0200

    login-page css

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,3 +1,5 @@
+import SignUpForm from "../components/sign-up_form/index";
+
 /**
  * Contains form for registering a new user profile.
  * @see https://docs.noroff.dev/social-endpoints/authentication
@@ -5,7 +7,7 @@
 export default function RegisterPage() {
   return (
     <>
-      <h1>Register Page</h1>
+      <SignUpForm></SignUpForm>
     </>
   );
 }
