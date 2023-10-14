ee761593ae79b15a6c032b1c57b04f85b820c91b
Khadarrr <111360349+Khadarrr@users.noreply.github.com>
2023-10-14 14:47:15 +0200

    improvments on profilepage

--- src/components/profile-page/index.jsx
+++ src/components/profile-page/index.jsx
@@ -45,43 +45,45 @@ export default function ProfilePage() {
 
   return (
     <div className="w-full">
-      {/* Profile Section */}
-      <div className="bg-neutral-100 text-gray-900 dark:text-white border-2 border-white dark:bg-gray-800 dark:border-gray-700 rounded-3xl overflow-hidden">
-        <div
-          className={`relative bg-indigo-600 h-40`}
-          style={{ backgroundColor: "#F4D196" }}
-        >
-          <img
-            src={profile?.banner}
-            alt="Banner"
-            className="w-full h-full object-cover"
-          />
-          <div className="absolute inset-0 bg-gradient-to-r from-indigo-600 to-indigo-400 opacity-50"></div>
-          <div className="absolute inset-0 flex items-center justify-center">
-            <img
-              src={profile?.avatar}
-              alt="Avatar"
-              className="w-32 h-32 rounded-full object-cover border-4 border-white"
-            />
-          </div>
-        </div>
-        {/* Profile Details */}
-        <div className="px-6 py-8">
-          <h1 className="text-xl sm:text-2xl md:text-3xl lg:text-3xl font-semibold text-gray-800 dark:text-white">
-            {profile?.name}
-          </h1>
-          <p className="text-gray-600 dark:text-white">Bio</p>
-        </div>
-        {/* Bio */}
-        <div className="px-6 py-4">
-          <h2 className="text-base sm:text-lg md:text-xl font-semibold text-gray-800 dark:text-white">
-              {profile?.email}
-          </h2>
-          <p className="text-sm sm:text-sm md:text-sm text-gray-600 dark:text-white">
-            I DANCE FOR MONEY
-          </p>
-        </div>
+  {/* Profile Section */}
+  <div className="bg-neutral-100 text-gray-900 dark:text-white border-2 border-white dark:bg-gray-800 dark:border-gray-700 rounded-3xl overflow-hidden">
+    <div className="relative h-60 lg:h-80">
+      <div
+        className="absolute inset-0 bg-gradient-to-r from-indigo-600 to-indigo-400 opacity-50"
+      ></div>
+      <img
+        src={profile?.banner}
+        alt="Banner"
+        className="w-full h-full object-cover"
+      />
+      <div className="absolute inset-0 flex items-center justify-center">
+        <img
+          src={profile?.avatar}
+          alt="Avatar"
+          className="w-32 h-32 lg:w-32 lg:h-48 rounded-full object-cover border-4 border-white"
+        />
       </div>
     </div>
+    {/* Profile Details */}
+    <div className="px-6 py-8">
+      <h1 className="text-2xl md:text-3xl lg:text-3xl font-semibold text-gray-800 dark:text-white">
+        {profile?.name}
+      </h1>
+      <p className="text-gray-600 dark:text-white ">
+        "I DANCE FOR MONEY"
+      </p>
+    </div>
+    if you're a cat looking for a dog who'll cherish your every whisker and meow
+    <div className="px-6 py-4">
+      <h2 className="text-lg md:text-xl font-semibold text-gray-800 dark:text-white">
+        {profile?.email}
+      </h2>
+      <p className="text-sm sm:text-base text-gray-600 dark:text-white">
+        {/* Add your bio content here */}
+      </p>
+    </div>
+  </div>
+</div>
+
   );
 }
