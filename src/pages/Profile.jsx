806ce85c962d735a255918ef3a02ab58182e4177
Cnbergh <110989342+Cnbergh@users.noreply.github.com>
2023-10-15 12:07:33 +0200

    Cleanup code to be able to deploy

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -2,12 +2,12 @@ import Header from "../components/navbar/header";
 import NavBar from "../components/navbar/home-nav";
 import ProfilePage from "../components/profile-page";
 import SingelPost from "../components/create-post";
-import Sosials from "../components/sosials-profile";
+import Sosials from "../components/Sosials-profile/index";
 
 export default function Profile() {
   return (
     <>
-      <div className="container mt-2 mx-2 w-full">
+      <div className="container w-full mx-2 mt-2">
         {/* Header*/}
         <Header />
 
@@ -19,15 +19,15 @@ export default function Profile() {
           </div>
 
           {/* Middle */}
-          <div className="w-10/12 sm:w-10/12 md:w-7/12 mx-1">
+          <div className="w-10/12 mx-1 sm:w-10/12 md:w-7/12">
             <ProfilePage />
             <SingelPost />
           </div>
 
           {/* Right Side */}
           <div className="w-0 sm:w-0 md:w-2/12">
-            
-            <Sosials/>
+
+            <Sosials />
           </div>
         </div>
       </div>
