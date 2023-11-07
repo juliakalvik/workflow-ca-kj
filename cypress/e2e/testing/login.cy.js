852f8f471b452af191f657dcd51ae0a0210b180f
Khadarrr <111360349+Khadarrr@users.noreply.github.com>
2023-11-07 11:17:32 +0100

    added cypress form test

--- cypress/e2e/testing/login.cy.js
+++ cypress/e2e/testing/login.cy.js
@@ -4,10 +4,10 @@ describe('Logging', () => {
   
       cy.scrollTo(0, 500);
   
-      // Select the button using its class name
+  
       cy.get('button.hidden').click();
   
-      // Make sure the input field with id "loginEmail" is visible and type in the email.
+
       cy.get('button').click();
 
       cy.get('img[alt="User Avatar"]').click();
